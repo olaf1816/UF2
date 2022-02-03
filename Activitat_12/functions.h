1b19ef5459b79a69ce4e2683b3cf273c8e1ae92f
olaf1816 <95416782+olaf1816@users.noreply.github.com>
2022-02-03 20:20:06 +0100

    Add files via upload

---
+++ Activitat_12/functions.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#define SIZE 100
+
+void validar();
+void rellenar(int);
+int validacion();
+void mostrar();
+
+#endif /* FUNCTIONS_H_ */
---
+++ Activitat_12/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: 27 ene 2022
+ *      Author: oalegre
+ */
+
+#include <stdio.h>
+#include "functions.h"
+
+void main(){
+
+	validar();
+
+}
